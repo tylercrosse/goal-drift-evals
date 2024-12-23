7bf56ee344509fd7c03a55b8da01e765e99d3d1e
Rauno Arike <r.arike@student.tudelft.nl>
2024-12-23 17:48:35 +0200

    Add plotting code for ablations

--- plot_scaling_laws.py
+++ plot_scaling_laws.py
@@ -22,18 +22,21 @@ def get_stats(data, clamp=False):
 def load_data():
     """Load data for a specific model and its variants."""
     paths = [
-        f'scaling_laws/hp_2/results.json',
-        f'scaling_laws/hp_4/results.json',
-        f'scaling_laws/hp_8/results.json',
+        f'scaling_laws/ep_2/results.json',
+        f'scaling_laws/ep_4/results.json',
+        f'scaling_laws/ep_8/results.json',
         # f'scaling_laws/sonnet_16/results.json',
-        f'scaling_laws/hp_gpt_2_ood/results.json',
-        f'scaling_laws/hp_gpt_4_ood/results.json',
-        f'scaling_laws/hp_gpt_8_ood/results.json',
+        f'scaling_laws/ep_gpt_2_ood/results.json',
+        f'scaling_laws/ep_gpt_4_ood/results.json',
+        f'scaling_laws/ep_gpt_8_ood/results.json',
         # f'scaling_laws/hp_sonnet_16_gpt/results.json',
-        f'scaling_laws/hp_gpt_2_baseline/results.json',
-        f'scaling_laws/hp_gpt_4_baseline/results.json',
-        f'scaling_laws/hp_gpt_8_baseline/results.json',
+        f'scaling_laws/ep_gpt_2_baseline/results.json',
+        f'scaling_laws/ep_gpt_4_baseline/results.json',
+        f'scaling_laws/ep_gpt_8_baseline/results.json',
         # f'scaling_laws/hp_16_multiple_source/baseline.json',
+        f'scaling_laws/ep_2_sysprompt_ablation/results.json',
+        f'scaling_laws/ep_4_sysprompt_ablation/results.json',
+        f'scaling_laws/ep_8_sysprompt_ablation/results.json',
     ]
     
     data = []
@@ -88,6 +91,13 @@ def plot_intrinsification():
         # '16_steps': calculate_last_10_mean_with_ci('sonnet', extract_emissions(data[9], 'gpt-4o-mini')),
     }
 
+    ablation_results_sysprompt = {
+        '2_steps': calculate_last_10_mean_with_ci('gpt-4o-mini', extract_emissions(data[9], 'gpt-4o-mini')),
+        '4_steps': calculate_last_10_mean_with_ci('gpt-4o-mini', extract_emissions(data[10], 'gpt-4o-mini')),
+        '8_steps': calculate_last_10_mean_with_ci('gpt-4o-mini', extract_emissions(data[11], 'gpt-4o-mini')),
+        # '16_steps': calculate_last_10_mean_with_ci('sonnet', extract_emissions(data[12], 'gpt-4o-mini')),
+    }
+
     # Calculate values for both conditions
     x_values = [2, 4, 8]
     
@@ -123,8 +133,32 @@ def plot_intrinsification():
         baseline_ci_lower.append(lower)
         baseline_ci_upper.append(upper)
 
+    ablation_results_sysprompt_differences = []
+    ablation_results_sysprompt_ci_lower = []
+    ablation_results_sysprompt_ci_upper = []
+    for steps in [2, 4, 8]:
+        key = f'{steps}_steps'
+        mean, lower, upper = ablation_results_sysprompt[key]
+        ablation_results_sysprompt_differences.append(mean)
+        ablation_results_sysprompt_ci_lower.append(lower)
+        ablation_results_sysprompt_ci_upper.append(upper)
+
     # Create plot
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 6))
+    
+    # Plot normal line
+    plt.errorbar(x_values, normal_differences, 
+                yerr=[np.array(normal_differences) - np.array(normal_ci_lower), 
+                      np.array(normal_ci_upper) - np.array(normal_differences)],
+                fmt='o-', color='blue', 
+                label='Instrumental goal conditioning', capsize=5, capthick=1, elinewidth=1)
+    
+    # Plot sysprompt ablation line
+    plt.errorbar(x_values, ablation_results_sysprompt_differences, 
+                yerr=[np.array(ablation_results_sysprompt_differences) - np.array(ablation_results_sysprompt_ci_lower), 
+                      np.array(ablation_results_sysprompt_ci_upper) - np.array(ablation_results_sysprompt_differences)],
+                fmt='o-', color='orange',
+                label='Instrumental goal conditioning, system prompt given in user message', capsize=5, capthick=1, elinewidth=1)
     
     # Plot single source line
     plt.errorbar(x_values, ss_differences, 
@@ -132,13 +166,6 @@ def plot_intrinsification():
                       np.array(ss_ci_upper) - np.array(ss_differences)],
                 fmt='o-', color='red',
                 label='Conditioned on random text', capsize=5, capthick=1, elinewidth=1)
-    
-    # Plot normal line
-    plt.errorbar(x_values, normal_differences, 
-                yerr=[np.array(normal_differences) - np.array(normal_ci_lower), 
-                      np.array(normal_ci_upper) - np.array(normal_differences)],
-                fmt='o-', color='blue', 
-                label='Conditioned on 4o-mini', capsize=5, capthick=1, elinewidth=1)
     
     # Plot baseline line
     plt.errorbar(x_values, baseline_differences, 
@@ -148,9 +175,10 @@ def plot_intrinsification():
                 label='Baseline', capsize=5, capthick=1, elinewidth=1)
     
     plt.xscale('log')
-    plt.title('Effect of Instrumental Steps on Goal Drift Score (4o-mini, health externality minimization -> profit maximization)')
+    plt.title('Effect of Instrumental Steps on Goal Drift Score\n(4o-mini, emission minimization -> profit maximization)')
     plt.ylabel('Goal drift score')
-    plt.ylim(-0.1, 1.1)
+    plt.ylim(-0.1, 1.3)
+    plt.yticks(np.arange(0, 1.1, 0.1))
     plt.xlabel('Number of instrumental steps')
     plt.xticks([2, 4, 8], ['2¹', '2²', '2³'])
     plt.gca().minorticks_off()
